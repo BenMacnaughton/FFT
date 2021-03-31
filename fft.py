8cf314680b3cdc1ab52970073e654035158997f9
aidanwilliams09 <aidan19@me.com>
2021-03-31 18:26:48 -0400

    fft done not tested

--- fft.py
+++ fft.py
@@ -4,22 +4,41 @@ from math import exp, pi
 import sys
 import os
 
-def FFT(signals):
-    pass
+
 
 def DFT(signals):
-    N, M = signals.shape
-    transform = np.zeros((N, M))
-    exponent = exp(-2J * pi)
+    # get the length of the array
+    N = signals.shape[0]
+    # create array from 1 to N-1
+    n = np.arange(N)
+    # create vertical array from 1 to N-1
+    k = np.reshape((N, 1))
+    # 2-D array of all the exponents
+    exponent = np.exp(-2J * np.pi * k * n / N)
+    # Adds from 0 to N-1 each each signal by the resultant exponents
+    return np.dot(exponent, signals)
 
-    for n in range(N):
-        for m in range(M):
-            for l in range(N):
-                for k in range(M):  
-                    transform[n, m] += signals[n, m]* exponent**(k*m/M + l*n/N)
+def FFT(signals):
+    N = signals.shape[0]
+
+    if N % 2 != 0:
+        raise ValueError("must be a power of two")
     
-    return transform
+    # set limit of 2 before performiing DFT on a signal
+    elif N <= 2:
+        return DFT(signals)
     
+    else:
+        # even and odd arrays
+        evens = FFT(signals[::2])
+        odds = FFT(signals[1::2])
+        # array of extra exponents for the odd part
+        extra_e = np.exp(-2J * np.pi * np.arange(N) / N)
+        # need arrays to be same size
+        # X_k where 0 <= k <= (N-1) / 2 done first
+        # second half done after
+        return np.concatenate(evens + extra_e[:int(N / 2)] * odds, evens + extra_e[int(N / 2):] * odds)
+
 if __name__ == "__main__":
     filename = "moonlanding.png"
     mode = 1
